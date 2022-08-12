9bca93c1af957905283ceb6e94954721b5e16f1d
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-12 13:14:06 +0530

    EXplanation

--- src/main/java/KannanandGajaCoreJava/FundamentalJava/Sample.java
+++ src/main/java/KannanandGajaCoreJava/FundamentalJava/Sample.java
@@ -4,7 +4,11 @@ public class Sample {
 		
 	public static void main(String[] args)
 	{
+		
 		System.out.println("Hello java.......987654345678;'[]976-[hkpi");//print statement	
+		
+		
+		
 		System.out.println("i am manojkumar,gaja,kannan");
 	}
 }
