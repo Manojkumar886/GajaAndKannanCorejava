db259f1fe4c75666dff6c224cbec304caef5fd0f
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-12 13:09:02 +0530

    First commit

--- src/main/java/KannanandGajaCoreJava/FundamentalJava/Sample.java
+++ src/main/java/KannanandGajaCoreJava/FundamentalJava/Sample.java
@@ -5,6 +5,6 @@ public class Sample {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello java.......987654345678;'[]976-[hkpi");//print statement	
-		
+		System.out.println("i am manojkumar,gaja,kannan");
 	}
 }
