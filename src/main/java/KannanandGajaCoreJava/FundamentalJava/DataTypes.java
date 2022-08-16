32991db8a65e3db1f99b646233fdaf7f85914206
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-16 15:12:02 +0530

    Datatypes Over

---
+++ src/main/java/KannanandGajaCoreJava/FundamentalJava/DataTypes.java
@@ -0,0 +1,25 @@
+package KannanandGajaCoreJava.FundamentalJava;
+
+public class DataTypes 
+{
+	public static void main(String[] args) //main method,thread
+	{
+		int ipnumber=34567898;//variable and value creation
+		long mobileno=9789564647l;
+		short smallest=8976;
+		float float1=23.65f;
+		double doub=89;
+		String name="Gaja@222.com";
+		char onlyone='A';
+		System.out.println("First is charcter"+onlyone +" My name is"+name +"i have used double"+doub);
+	
+		System.out.println("my name is : "+name);
+		System.out.println(smallest+" -this is short number");
+		System.out.println("this is ipnumber in my laptop :"+ipnumber);
+		System.out.println("This is mobile no : "+mobileno);
+		
+		
+	}
+	
+
+}
